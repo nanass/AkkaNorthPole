5a6e524e441fb05ad25c24dd1cda55abd841962b
Nathan Nass <nathan.nass@gmail.com>
2013-11-23 13:18:42 -0600

    Initial Actor Implementation Commit

---
+++ src/main/java/AkkaNorthPole/Actors/Santa.java
@@ -0,0 +1,113 @@
+package AkkaNorthPole.Actors;
+
+import AkkaNorthPole.Messages.Msg;
+import AkkaNorthPole.Messages.NorthPoleMsg;
+import akka.actor.ActorRef;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Santa extends NorthPoleActor{
+
+    enum santaState implements State {awake, asleep, welcomingElves,
+                                      consultingWithElves, dismissingElves,
+                                      hitchingReindeer, deliveringToys,
+                                      dismissingReindeer}
+    int wait = 0;
+    private ActorRef waitingRoom;
+
+    private HashMap<String, Msg> visitorQueue = new HashMap<String, Msg>();
+    public Santa(String name) {
+        super(name);
+        setState(santaState.asleep);
+        log("Sleeping");
+    }
+    @Override
+    public void onReceive(Object message) throws Exception {
+        Msg msg = (Msg)message;
+        switch (msg.nPMsg) {
+            case WakeUp :
+                 visitorQueue.put(msg.group, msg);
+                 if(getState().equals(santaState.asleep)) {
+                     if(visitorQueue.containsKey("Reindeer")) {
+                        if(msg.group.equals("Elves")){
+                            getSender().tell(new Msg(NorthPoleMsg.Wait, msg.group, msg.who), getSelf());
+                        }
+                        handleVisitors(visitorQueue.get("Reindeer"));
+                        visitorQueue.remove("Reindeer");
+                     }
+                     else {
+                        handleVisitors(visitorQueue.get("Elves"));
+                        visitorQueue.remove("Elves");
+                     }
+                 }
+                 else {
+                     getSender().tell(new Msg(NorthPoleMsg.Wait, msg.group, msg.who), getSelf());
+                 }
+                break;
+            case Done :
+                if (wait > 1) {
+                    wait--;
+                }
+                else {
+                    switch((santaState)getState()){
+                        case welcomingElves :
+                            log("Welcoming elves");
+                            setState(santaState.consultingWithElves);
+                            doAction(msg.who, new Msg(NorthPoleMsg.Consult, msg.group, msg.who));
+                            break;
+                        case consultingWithElves :
+                            log("Consulting with elves");
+                            setState(santaState.dismissingElves);
+                            doAction(msg.who, new Msg(NorthPoleMsg.GoodBye, msg.group, msg.who));
+                            break;
+                        case dismissingElves :
+                            log("Sleeping");
+                            setState(santaState.asleep);
+                            waitingRoom.tell(new Msg(NorthPoleMsg.Dismiss, msg.group, msg.who),getSelf());
+                            break;
+                        case hitchingReindeer :
+                            log("Harnessing reindeer");
+                            setState(santaState.deliveringToys);
+                            doAction(msg.who, new Msg(NorthPoleMsg.Deliver, msg.group, msg.who));
+                            break;
+                        case deliveringToys :
+                            log("Delivering Toys");
+                            setState(santaState.dismissingReindeer);
+                            doAction(msg.who, new Msg(NorthPoleMsg.Unhitch, msg.group, msg.who));
+                            break;
+                        case dismissingReindeer :
+                            log("Unharnessing reindeer");
+                            log("Sleeping");
+                            setState(santaState.asleep);
+                            waitingRoom.tell(new Msg(NorthPoleMsg.Dismiss, msg.group, msg.who),getSelf());
+                            break;
+                    }
+                }
+                break;
+            case WaitingRoom :
+                waitingRoom = getSender();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void doAction(List<ActorRef> who, Msg what){
+        wait = who.size();
+        batchSend(who,what);
+    }
+
+    private void handleVisitors(Msg msg){
+        setState(santaState.awake);
+        log("Waking up");
+        if(msg.group.equals("Elves")){
+            setState(santaState.welcomingElves);
+            doAction(msg.who, new Msg(NorthPoleMsg.ComeIn, msg.group, msg.who));
+        }
+        else if(msg.group.equals("Reindeer")){
+            setState(santaState.hitchingReindeer);
+            doAction(msg.who, new Msg(NorthPoleMsg.Hitch, msg.group, msg.who));
+        }
+    }
+}
---
+++ src/main/java/AkkaNorthPole/Main/NorthPole.java
@@ -0,0 +1,35 @@
+package AkkaNorthPole.Main;
+
+import AkkaNorthPole.Actors.*;
+import AkkaNorthPole.Messages.Msg;
+import AkkaNorthPole.Messages.NorthPoleMsg;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+class NorthPole{
+
+    enum name{ Dasher, Dancer, Prancer, Vixen, Comet, Cupid, Donder, Blitzen, Ruldolph }
+
+    public static void main(String[] args) throws IOException {
+
+        List<ActorRef> elves = new ArrayList<ActorRef>();
+        List<ActorRef> reindeer = new ArrayList<ActorRef>();
+
+        ActorSystem system = ActorSystem.create("NorthPole");
+        ActorRef santa = system.actorOf(Props.create(Santa.class, "Santa"));
+        ActorRef waitingRoom = system.actorOf(Props.create(WaitingRoom.class, "WaitingRoom", santa));
+        santa.tell(new Msg(NorthPoleMsg.WaitingRoom), waitingRoom);
+
+        for (int i = 1; i <= 10; i++){
+            elves.add(system.actorOf(Props.create(Elf.class, "Elf" + String.valueOf(i), waitingRoom)));
+        }
+        for (name r : name.values()){
+            reindeer.add(system.actorOf(Props.create(Reindeer.class, r.toString(), waitingRoom)));
+        }
+    }
+}
